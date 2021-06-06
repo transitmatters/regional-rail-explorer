74efce3875f219c58384f99cb1d97897245137b4
kaylalee <kaylalee@umich.edu>
2021-06-06 16:32:32 -0400

    working S.J. for 1+ journeys

--- src/components/SuggestedJourneys/SuggestedJourneys.stories.tsx
+++ src/components/SuggestedJourneys/SuggestedJourneys.stories.tsx
@@ -1,12 +1,46 @@
 import React from "react";
+import { JourneyParams } from "types";
 
 import SuggestedJourneys from "./SuggestedJourneys";
+
 
 export default {
     title: "SuggestedJourneys",
     component: SuggestedJourneys,
 };
 
+const journeyParams1: JourneyParams = { fromStationId: "place-NEC-2173", toStationId: "place-bbsta",
+                                        day: "weekday", time: 32010};
+const journeyParams2: JourneyParams = { fromStationId: "place-DB-2222", toStationId: "place-sstat",
+                                        day: "weekday", time: 32010};
+
+// const journeyQueue: journeyMap = [
+//     {
+//         params: journeyParams1,
+//         fromStationName: "Route 128",
+//         toStationName: "Back Bay",
+//     },
+//     {
+//         params: journeyParams2,
+//         fromStationName: "Blue Hill Avenue",
+//         toStationName: "South Station",
+//     }
+// ]
+const journey = [
+    {
+        params: journeyParams1,
+        fromStationName: "Route 128", 
+        toStationName: "Back Bay"
+    },
+    {
+        params: journeyParams2,
+        fromStationName: "Blue Hill Avenue", 
+        toStationName: "South Station"
+    }
+];
+
+
+
 export const Default = () => {
-    return <SuggestedJourneys journey1 = {{ origin: "Route 128", destination: "Back Bay"}} />
+    return <SuggestedJourneys journeys = { journey } />
 };
--- src/components/SuggestedJourneys/SuggestedJourneys.tsx
+++ src/components/SuggestedJourneys/SuggestedJourneys.tsx
@@ -1,31 +1,51 @@
 import React from "react";
-
+import { stationsById } from "stations";
+import { JourneyParams } from "types";
 
 import styles from "./SuggestedJourneys.module.scss";
 
-interface j1 {
-    origin: string;
-    destination: string;
+interface journey {
+    // origin: string;
+    // destination: string;
+    // originStationId: JourneyParams["fromStationId"];
+    // destStationId: JourneyParams["toStationId"];
+    // day: JourneyParams["day"];
+    // time: JourneyParams["time"];
+    params: JourneyParams;
+    fromStationName: string;
+    toStationName: string;
 }
 
 type Props = { 
-    journey1: j1;
+    journeys: journey[];
 }
 
-
+export function getJourneyUrl(fromStationId: string, toStationId: string, day: string, time: number | undefined) {
+    return `/?from=${fromStationId}&to=${toStationId}&day=${day}&time=${time}`;
+}
 
 const SuggestedJourneys = (props: Props) => {
-    const { journey1 } = props;
-    return (
-
-    <div className={styles.journey1}> 
-        {/* <Button onClick={action("click")} large></Button> */}
-        <a href="/?from=place-NEC-2173&to=place-bbsta&day=weekday&time=32010"><h2>Journey Option 1:</h2>
-        <p>Depart from: { journey1.origin }
-        <p></p>Arrive at: { journey1.destination }</p></a>
-    </div>
-    
-    );
+    const { journeys } = props;
+    let divArr = [] as any;
+    for (let i = 0; i < journeys.length; ++i) {
+        divArr.push(
+            <div className={styles.journey}> 
+                 <a href={getJourneyUrl(journeys[i].params["fromStationId"], journeys[i].params["toStationId"], journeys[i].params["day"], journeys[i].params["time"])}>
+                 <h2>Journey Option {i + 1}:</h2>
+                 <p>Depart from: { journeys[i].fromStationName }</p>
+                 <p>Arrive at: { journeys[i].toStationName }</p></a>
+             </div>
+        );
+    } 
+    return divArr;
+    // return (
+    // <div className={styles.journey1}> 
+    //     {/* <Button onClick={action("click")} large></Button> */}
+    //     <a href={getJourneyUrl(journeys[i].params["fromStationId"], journeys.params["toStationId"], journeys.params["day"], journeys.params["time"])}><h2>Journey Option 1:</h2>
+    //     <p>Depart from: { journeys.fromStationName }
+    //     <p></p>Arrive at: { journeys.toStationName }</p></a>
+    // </div>
+    // );  
 };
 
 export default SuggestedJourneys; 
