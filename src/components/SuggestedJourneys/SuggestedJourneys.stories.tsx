ebe9e04b815cd770300be24c6f4782cebb4a2195
kaylalee <kaylalee@umich.edu>
2021-06-14 21:04:12 -0400

    butons change color based on time of day

--- src/components/SuggestedJourneys/SuggestedJourneys.stories.tsx
+++ src/components/SuggestedJourneys/SuggestedJourneys.stories.tsx
@@ -19,10 +19,17 @@ const journeyParams2: JourneyParams = {
     fromStationId: "place-DB-2222",
     toStationId: "place-sstat",
     day: "weekday",
-    time: 32010,
+    time: 51300,
 };
 
-const journey = [journeyParams1, journeyParams2];
+const journeyParams3: JourneyParams = {
+    fromStationId: "place-KB-0351",
+    toStationId: "place-PB-0356",
+    day: "weekday",
+    time: 73358,
+};
+
+const journey = [journeyParams1, journeyParams2,journeyParams3];
 
 export const Default = () => {
     return <SuggestedJourneys journeys={journey} />;
--- src/components/SuggestedJourneys/SuggestedJourneys.tsx
+++ src/components/SuggestedJourneys/SuggestedJourneys.tsx
@@ -12,18 +12,32 @@ export function getJourneyUrl(journey: JourneyParams) {
     return `/?from=${journey["fromStationId"]}&to=${journey["toStationId"]}&day=${journey["day"]}&time=${journey["time"]}`;
 }
 
+function getClass(time: JourneyParams) {
+    let times = time["time"];
+    if (times === undefined) {
+        return styles.midday;
+    }else if (times >= 18000 && times <= 39600) {
+        return styles.morning;
+    }else if (times > 39600 && times <= 61200) {
+        return styles.midday;
+    }else return styles.evening; 
+}
+
 const SuggestedJourneys = (props: Props) => {
     const { journeys } = props;
     const divArr = [] as any;
     for (let i = 0; i < journeys.length; ++i) {
         divArr.push(
             <div className={styles.journey}>
-                <a href={getJourneyUrl(journeys[i])}>
-                    <h2>Journey Option {i + 1}:</h2>
-                    <p>Depart from: {stationsById[journeys[i]["fromStationId"]].name}</p>
-                    <p>Arrive at: {stationsById[journeys[i]["toStationId"]].name}</p>
-                </a>
-            </div>
+                <div className={getClass(journeys[i])}>
+                    <a href={getJourneyUrl(journeys[i])} >
+                        <h2>Journey Option {i + 1}:</h2>
+                        <p>Depart from: {stationsById[journeys[i]["fromStationId"]].name}</p>
+                        <p>Arrive at: {stationsById[journeys[i]["toStationId"]].name}</p>
+                    </a>
+                </div>
+             </div>
+
         );
     }
     return divArr;
