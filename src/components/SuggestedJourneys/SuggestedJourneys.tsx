d945d9eff394345b11a50fc8729782a1612c581a
kaylalee1116 <68870737+kaylalee1116@users.noreply.github.com>
2021-06-16 22:51:14 -0400

    Color Changing Buttons according to time of day (#20)
    * Kayla's work
    
    * working S.J. for 1+ journeys
    
    * style updates
    
    * SJ style edits
    
    * got rid of css prefixes
    
    * buttons change color based on time of day
    
    * color-changing buttons based on time of day
    
    * recommended changes from ian
    
    * final edits
    
    * minor style changes
    
    Co-authored-by: kaylalee <kaylalee@umich.edu>

--- src/components/SuggestedJourneys/SuggestedJourneys.tsx
+++ src/components/SuggestedJourneys/SuggestedJourneys.tsx
@@ -12,17 +12,30 @@ export function getJourneyUrl(journey: JourneyParams) {
     return `/?from=${journey["fromStationId"]}&to=${journey["toStationId"]}&day=${journey["day"]}&time=${journey["time"]}`;
 }
 
+function getTimeOfDayClass(params: JourneyParams) {
+    const { time } = params;
+    if (time === undefined) {
+        return styles.midday;
+    } else if (time >= 18000 && time <= 39600) {
+        return styles.morning;
+    } else if (time > 39600 && time <= 61200) {
+        return styles.midday;
+    } else return styles.evening;
+}
+
 const SuggestedJourneys = (props: Props) => {
     const { journeys } = props;
     const divArr = [] as any;
     for (let i = 0; i < journeys.length; ++i) {
         divArr.push(
-            <div className={styles.journey}>
-                <a href={getJourneyUrl(journeys[i])}>
-                    <h2>Journey Option {i + 1}:</h2>
-                    <p>Depart from: {stationsById[journeys[i]["fromStationId"]].name}</p>
-                    <p>Arrive at: {stationsById[journeys[i]["toStationId"]].name}</p>
-                </a>
+            <div className={styles.journey} key={i}>
+                <div className={getTimeOfDayClass(journeys[i])}>
+                    <a href={getJourneyUrl(journeys[i])}>
+                        <h2>Journey Option {i + 1}:</h2>
+                        <p>Depart from: {stationsById[journeys[i]["fromStationId"]].name}</p>
+                        <p>Arrive at: {stationsById[journeys[i]["toStationId"]].name}</p>
+                    </a>
+                </div>
             </div>
         );
     }
