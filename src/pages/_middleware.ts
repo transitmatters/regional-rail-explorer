e3492ace742b74f20d119ae074706da86a9f6508
Ian Reynolds <idreyn@gmail.com>
2022-04-07 12:02:01 -0400

    literally just log to see what's there

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -1,14 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export default (req: NextRequest) => {
-    if (process.env.NODE_ENV === "production") {
-        const { headers, url } = req;
-        const isNotHttps = headers.get("x-forwarded-proto") !== "https";
-        if (isNotHttps && url.startsWith("http://")) {
-            const restOfUrl = url.slice(7);
-            const redirectUrl = `https://${restOfUrl}`;
-            return NextResponse.redirect(redirectUrl, 301);
-        }
-    }
+    console.log(req.nextUrl);
     return NextResponse.next();
 };
