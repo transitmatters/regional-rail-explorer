6c4b1457e3322d2b690612a1ae3ec2c6d1e4e608
Ian Reynolds <idreyn@gmail.com>
2022-04-07 10:59:41 -0400

    Title and favicon!

--- src/components/AppFrame/AppFrame.tsx
+++ src/components/AppFrame/AppFrame.tsx
@@ -21,14 +21,7 @@ const AppFrame = (props: Props) => {
     const { globalNavRef, controlsRef } = useAppContext();
     return (
         <div className={styles.appFrame}>
-            <Head>
-                {meta}
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
-                />
-                <title>Regional Rail Explorer</title>
-            </Head>
+            <Head>{meta}</Head>
             <GlobalNav ref={globalNavRef} />
             {controls && (
                 <div className={styles.controls} ref={controlsRef}>
