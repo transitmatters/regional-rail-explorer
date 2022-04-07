54cac21dfbd35d4b4db7bb5b6bbfa85a533c0c0d
Ian Reynolds <idreyn@gmail.com>
2022-04-07 11:49:21 -0400

    write our own SSL middleware since the existing one seems broken

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -1,3 +1,14 @@
-import sslRedirect from "next-ssl-redirect-middleware";
+import { NextRequest, NextResponse } from "next/server";
 
-export default sslRedirect({});
+export default (req: NextRequest) => {
+    if (process.env.NODE_ENV === "production") {
+        const { headers, url } = req;
+        const isNotHttps = headers.get("x-forwarded-proto") !== "https";
+        if (isNotHttps && url.startsWith("http://")) {
+            const restOfUrl = url.slice(7);
+            const redirectUrl = `https://${restOfUrl}`;
+            return NextResponse.redirect(redirectUrl, 301);
+        }
+    }
+    return NextResponse.next();
+};
