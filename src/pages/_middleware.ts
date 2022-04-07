1eed5a2048f83ad2bd3d511d3764af17a361faf0
Ian Reynolds <idreyn@gmail.com>
2022-04-07 14:19:42 -0400

    really think I figured it out

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -1,11 +1,11 @@
 import { sslMiddleware } from "middleware/ssl";
 
 const getHost = () => {
-    if (process.env.HEROKU_APP_NAME) {
-        return `${process.env.HEROKU_APP_NAME}.herokuapp.com`;
-    }
     if (process.env.PRODUCTION_HOST) {
         return process.env.PRODUCTION_HOST;
+    }
+    if (process.env.HEROKU_APP_NAME) {
+        return `${process.env.HEROKU_APP_NAME}.herokuapp.com`;
     }
     return null;
 };
