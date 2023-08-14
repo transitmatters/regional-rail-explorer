a446a610cf95f135760dff50ba6508756bba5120
Devin Matte <devin.matte@datadoghq.com>
2023-08-13 21:41:54 -0400

    Remove Heroku configs and update a few packages

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -4,9 +4,6 @@ const getHost = () => {
     if (process.env.PRODUCTION_HOST) {
         return process.env.PRODUCTION_HOST;
     }
-    if (process.env.HEROKU_APP_NAME) {
-        return `${process.env.HEROKU_APP_NAME}.herokuapp.com`;
-    }
     return null;
 };
 
