411b8eeab8f3e81b8488f70dde799f49eff753aa
Colby Haskell <chaskell35@gmail.com>
2025-02-17 16:46:45 -0500

    Update scenarios.json generation

--- next.config.ts
+++ next.config.ts
@@ -1,42 +1,26 @@
 import type { NextConfig } from "next";
 import webpack from "webpack";
-import fs from "fs";
-import path from "path";
-import memoize from "fast-memoize";
-import { loadScenarios } from "./src/server/_loadScenarios";
-import { stringify, parse } from "flatted";
+import { loadScenariosFromFile, writeScenariosToFile } from "./src/server/_loadScenarios";
 
 import { PHASE_PRODUCTION_SERVER, PHASE_DEVELOPMENT_SERVER } from "next/dist/shared/lib/constants";
 
-const loadScenariosFromFile = () => {
-    return fs.readFileSync("./scenarios.json", "utf8");
-};
-
-const createScenariosJsonString = memoize(() => {
-    return stringify(loadScenarios());
-});
-
-const writeScenariosFile = () => {
-    fs.writeFileSync("./scenarios.json", createScenariosJsonString());
-};
-
 class WriteScenariosPlugin {
     apply(compiler: webpack.Compiler) {
-        compiler.hooks.done.tap(WriteScenariosPlugin.name, writeScenariosFile);
+        compiler.hooks.done.tap(WriteScenariosPlugin.name, writeScenariosToFile);
     }
 }
 
-const getServerRuntimeConfig = (phase) => {
+const getServerRuntimeConfig = (phase: string) => {
     if (phase === PHASE_PRODUCTION_SERVER || PHASE_DEVELOPMENT_SERVER) {
-        return { scenarios: parse(loadScenariosFromFile()) };
+        return { scenarios: loadScenariosFromFile() };
     }
     return {};
 };
 
-const nextConfig: NextConfig = (phase) => {
+const nextConfig: NextConfig = (phase: string) => {
     return {
         serverRuntimeConfig: getServerRuntimeConfig(phase),
-        webpack: (config, options) => {
+        webpack: (config: any, options: any) => {
             if (!options.isServer) {
                 config.plugins.push(new WriteScenariosPlugin());
             }
--- src/server/_loadScenarios.ts
+++ src/server/_loadScenarios.ts
@@ -1,23 +1,66 @@
+import fs from "fs";
 import path from "path";
 
 import { loadGtfsNetwork } from "./network";
+import { stringify, parse } from "flatted";
 
-const gtfsNetwork = (networkName: string) =>
-    loadGtfsNetwork(path.resolve(process.cwd(), "data", networkName + ".tar.gz"));
+const getGtfsFilePath = (networkName: string): string => {
+    return path.resolve(process.cwd(), "data", networkName + ".tar.gz");
+};
 
-export const loadScenarios = () => {
-    return [
-        {
-            id: "present",
-            name: "Today's commuter rail",
-            unifiedFares: false,
-            network: gtfsNetwork("gtfs-present"),
-        },
-        {
-            id: "regional_rail",
-            name: "Electrified regional rail",
-            unifiedFares: true,
-            network: gtfsNetwork("gtfs-regional-rail"),
-        },
-    ];
+const getScenariosFilePath = () => {
+    return path.resolve(process.cwd(), "scenarios.json");
+};
+
+export const loadScenariosFromFile = () => {
+    return parse(fs.readFileSync("./scenarios.json", "utf8"));
+};
+
+const gtfsNetworksToRead = [
+    {
+        id: "present",
+        name: "Today's commuter rail",
+        unifiedFares: false,
+        networkName: "gtfs-present",
+    },
+    {
+        id: "regional_rail",
+        name: "Electrified regional rail",
+        unifiedFares: true,
+        networkName: "gtfs-regional-rail",
+    },
+];
+
+export const loadScenariosFromGtfs = () => {
+    return gtfsNetworksToRead.map((networkDetails) => {
+        return {
+            id: networkDetails.id,
+            name: networkDetails.name,
+            unifiedFares: networkDetails.unifiedFares,
+            network: loadGtfsNetwork(getGtfsFilePath(networkDetails.networkName)),
+        };
+    });
+};
+
+export const writeScenariosToFile = () => {
+    const scenariosFileIsUpToDate = gtfsNetworksToRead.every((network) => {
+        try {
+            const gtfsFileStat = fs.statSync(getGtfsFilePath(network.networkName));
+            const scenariosFileStat = fs.statSync(getScenariosFilePath());
+
+            if (gtfsFileStat.mtime < scenariosFileStat.mtime) {
+                return true;
+            }
+        } catch (err) {
+            return false;
+        }
+    });
+
+    // Skip update if gtfs data has not changed
+    if (scenariosFileIsUpToDate) {
+        return;
+    }
+
+    // Update scenarios.json if gtfs data has changed
+    fs.writeFileSync(getScenariosFilePath(), stringify(loadScenariosFromGtfs()));
 };
